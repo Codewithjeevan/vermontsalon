ad38cb9c176428904edb704a823bcfa70e5d90a2
Codewithjeevan <86449836+Codewithjeevan@users.noreply.github.com>
2025-03-20 20:00:37 +0530

    Added New Changes

---
+++ api_documentation/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Off POS API Documentation</title>
+    <link rel="stylesheet" type="text/css" href="swagger/swagger-ui.css" >
+    <link rel="stylesheet" type="text/css" href="swagger/index.css" >
+  </head>
+
+  <body>
+    <div id="swagger-ui"></div>
+
+    <script src="swagger/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script src="swagger/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <script src="swagger/index.js" charset="UTF-8"> </script>
+ 
+  </body>
+</html>
