76a8c8259eb97c4211cabc6bc576903455310b33
Jeevan Sahu <jeevansahu72290@gmail.com>
2025-07-17 22:32:42 +0530

    common_model & pos script is updated (#47)

--- frequent_changing/js/pos_script.js
+++ frequent_changing/js/pos_script.js
@@ -7236,7 +7236,7 @@ $(function () {
         return $.ajax({
             url: base_url + "Sale/getCustomersAjax",
             method: "GET",
-            data: { search: '', page: 1 }
+            data: { search: customer_id, page: 1 }
         })
         .done(function (response) {
             if (customer_id) {
@@ -7248,7 +7248,9 @@ $(function () {
                 true,  // selected
                 true   // defaultSelected
                 );
-                $sel.append(option).trigger('change');
+                if($sel.length > 0 && typeof $sel.append === 'function'){
+                    $sel.append(option).trigger('change');
+                }
             }
             }
         })
@@ -7303,7 +7305,8 @@ $(function () {
 
             $(document).ready(function () {
                 const $first = $('#walk_in_customer');
-                getAllCustomers($first, edit_sale_customer, false);
+                var defaultcustomer = edit_sale_customer ? edit_sale_customer : default_customer;
+                getAllCustomers($first, defaultcustomer, false);
             });
 
     
@@ -7408,7 +7411,8 @@ $(function () {
                             },
                             success: function (response) {
                                 if(response.status == 'success'){
-                                    getAllCustomers($('#walk_in_customer'), response.customer_id, true)
+                                    var walkcustomer = $('#walk_in_customer');
+                                    getAllCustomers(walkcustomer, response.customer_id, true)
                                     customerModalFieldRest();
                                     $('.loader1').slideUp('500');
                                 }else if(response.status == 'error'){
@@ -9384,8 +9388,11 @@ $(function () {
 
     // Code optimize by Azhar ** Final **
     function resetDefaultCustomer() {
-        let customer_id = $('#walk_in_customer > option:contains("Walk-in Customer")').attr('value');
-        $("#walk_in_customer").val(customer_id).trigger("change");
+        var walkncustomer = $('#walk_in_customer');
+        var defaultcustomer = default_customer;
+        getAllCustomers(walkncustomer, defaultcustomer, false);
+        // let customer_id = $('#walk_in_customer > option:contains("Walk-in Customer")').attr('value');
+        // $("#walk_in_customer").val(customer_id).trigger("change");
         $('#place_edit_order').text(Place_Order);
     }
 
@@ -9956,6 +9963,7 @@ $(function () {
         $('#send_invoice_sms').prop('checked', false);
         $('#send_invoice_email').prop('checked', false);
         $('#finalie_order_payment_method').css('border', '1px solid #B5D6F6');
+        resetDefaultCustomer();
     }
 
     
