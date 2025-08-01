e6edd1b599bf0d14adac08cccf42580430912da5
Jeevan Sahu <jeevansahu72290@gmail.com>
2025-08-01 22:38:18 +0530

    Printserviceschange (#58)
    * printing setting added
    
    * added automatically print functiality
    
    * print updated

--- application/views/sale/print_letter_head.php
+++ application/views/sale/print_letter_head.php
@@ -778,11 +778,10 @@ $inv_config = json_decode($invoice_configuration);
     <!-- <script src="<?php echo base_url(); ?>frequent_changing/js/onload_print.js"></script> -->
      <script>
             $(document).ready(function () {
-                setTimeout(() => {
-                    $('.print-btn').click();
-                }, 1000);
-            });
+                $('.print-btn').click();
+            }); 
      </script>  
+     
 </body>
 
 </html>
